ecacda6b4d23484318fa89be6a4d0a060dfc1be0
Mahima Sachdeva <32546808+mahi397@users.noreply.github.com>
2024-07-07 22:07:16 +0530

    import custom styles for nprogress

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,8 @@
 import NProgress from 'nprogress';
 import Router from 'next/router';
 import Page from '../components/Page';
-import 'nprogress/nprogress.css'; // styles for NProgress
+// import 'nprogress/nprogress.css'; // load standard styles for NProgress
+import '../components/styles/nprogress.css'; // custom styles for NProgress
 
 Router.events.on('routeChangeStart', () => NProgress.start()); // listen for route change start event
 Router.events.on('routeChangeComplete', () => NProgress.done());
